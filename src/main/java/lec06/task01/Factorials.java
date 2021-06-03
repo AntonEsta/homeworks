95b6757936d2dc355860e28a08d313e5d1d0b771
Anton Esta <a.v.esta@inbox.ru>
2021-06-03 16:27:20 +0700

    Homework #06. Threads. Created new branch. Added packages lec06.task05. Added file task.txt.

---
+++ src/main/java/lec06/task01/Factorials.java
@@ -0,0 +1,11 @@
+package lec06.task01;
+
+public class Factorials implements Runnable {
+
+    @Override
+    public void run() {
+
+        System.out.println("Hi!");
+
+    }
+}
---
+++ src/main/java/lec06/task01/Main.java
@@ -0,0 +1,8 @@
+package lec06.task01;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
