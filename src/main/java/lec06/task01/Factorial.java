a1f3c0ec7c68db4b0b7f518d7dcedcf5c7157360
Anton Esta <a.v.esta@inbox.ru>
2021-06-17 00:01:50 +0700

    Homework #06. Task01. Review code.

--- src/main/java/lec06/task01/Factorial.java
+++ src/main/java/lec06/task01/Factorial.java
@@ -1,39 +1,48 @@
 package lec06.task01;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.ToString;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Getter
 @EqualsAndHashCode
 @ToString
-public class Factorial<T extends BigInteger> implements Callable<T> {
+@FieldDefaults(level= AccessLevel.PUBLIC)
+public class Factorial implements Callable<BigInteger> {
 
-    private int n;
+    int n;
 
     public Factorial(@NonNull int n) {
         if (n < 1) return;
         this.n = n;
     }
 
-    public void setN(@NonNull int n) {
+    /**  */
+    public void setN(@NonNull int n) throws NullPointerException{
         if (n < 1) throw new NumberFormatException("Expected \"n > 0\"");
         this.n = n;
     }
 
+    public static Collection<BigInteger> getFactorials(Collection<BigInteger> values) {
+
+        ConcurrentLinkedQueue<BigInteger> queue = (ConcurrentLinkedQueue<BigInteger>) values;
+
+        queue.stream().forEach();
+
+    }
+
+    /**  Вычисляет факториал */
     @Override
-    public T call() {
-
+    public BigInteger call() {
         BigInteger result = BigInteger.valueOf(1);
-
         for (int i=2; i < n; i++) {
             result = result.multiply(BigInteger.valueOf(i));
         }
-
-        return (T) result;
+        return result;
     }
 }
--- src/main/java/lec06/task01/Factorials.java
+++ src/main/java/lec06/task01/Factorials.java
@@ -6,19 +6,33 @@ import lombok.experimental.UtilityClass;
 import java.math.BigInteger;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class Factorials {
 
-    public  List<? extends BigInteger> getFact(@NonNull Collection<? extends BigInteger> values) {
+    public static List<BigInteger> getFactorials(@NonNull Collection<BigInteger> values) throws InterruptedException {
 
-        ThreadPoolExecutor threads = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-        LinkedList<Callable<BigInteger>> callables = new LinkedList<>();
-        ArrayList<Future<BigInteger>> bigints = new ArrayList<>();
+        ExecutorService threads = Executors.newFixedThreadPool(values.size());
+        List<Callable<BigInteger>> callables = new ArrayList<>();
 
-        for (BigInteger value : values) {
-            callables.add(new Factorial<>(value.intValue()));
-        }
+
+//        List<Future<BigInteger>> bigints = new ArrayList<>();
+
+       /* for (BigInteger value : values) {
+            callables.add(new Factorial(value.intValue()));
+        }*/
+
+        /* TODO: continue here */
+        List<BigInteger> result = threads.invokeAll(callables).stream().collect(Collectors::toList());
+
+
+
+
+
+
+        Collection<Callable<BigInteger>> callables = new LinkedList<>();
+
 
         try {
             bigints = (ArrayList<Future<BigInteger>>) threads.invokeAll(callables);
@@ -41,4 +55,52 @@ public class Factorials {
 
         return result;
     }
+
+    public static void main(String[] args) {
+
+        /*TODO: уточнить тип коллекции*/
+        Set<BigInteger> values = new HashSet<>();
+
+        Random random = new Random();
+        int randomBound = 100;
+        final int valCount = 100;
+
+        while (values.size() < valCount) {
+            values.add(BigInteger.valueOf(random.nextInt(randomBound)));
+        }
+
+        List<Callable<BigInteger>> callables = new LinkedList<>();
+
+        for (BigInteger val : values) {
+            callables.add(new Factorial(val.intValue()));
+        }
+
+        ExecutorService pool = Executors.newFixedThreadPool(callables.size());
+
+        List<Future<BigInteger>> futures = new ArrayList<>();
+
+        try {
+            futures = pool.invokeAll(callables);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
+        pool.shutdown();
+
+        // Вывод результатов
+        values.forEach(System.out::println);
+
+        System.out.println();
+
+        for (Future<BigInteger> f : futures) {
+            try {
+                System.out.println(f.get());
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+
+        System.out.println(Factorials.getFactorials(values));
+    }
+
 }
